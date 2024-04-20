336826b86723b10511608d55897cca7509a6d30e
Ashish Kr. Pandey <akp9117409798@gmail.com>
2024-04-20 15:49:35 +0530

    responsive navBar created

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,10 @@ module.exports = {
     extend: {
       fontFamily: { 
         "Poppins": ['Poppins', 'sans-serif'] 
-    } 
+    },
+    colors: {
+      'my-custom-color': '#F97316', // Define your custom color here
+    },
     },
   },
   plugins: [],
