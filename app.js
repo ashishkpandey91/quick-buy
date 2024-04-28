a3cfbb1170955f565291762c5cae6aa804c87bcc
Ashish Kr. Pandey <akp9117409798@gmail.com>
2024-04-28 23:17:03 +0530

    First shoping card created

--- app.js
+++ app.js
@@ -3,8 +3,10 @@ const mobileNav = document.querySelector('#mobileNav');
 const crossNan = document.querySelector('.fa-xmark');
 
 menuBtn.addEventListener("click",()=>{
-    mobileNav.classList.remove("-translate-x-full");
+    mobileNav.classList.remove("-translate-x-full", "opecity-0");
+    // mobileNav.classList.remove();
 });
 crossNan.addEventListener("click",()=>{
-    mobileNav.classList.add("-translate-x-full");
+    mobileNav.classList.add("-translate-x-full", "opecity-0" );
+    // mobileNav.classList.add();
 });
