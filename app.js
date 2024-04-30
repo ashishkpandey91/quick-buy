97418beb3b4ba1237df8d6b4df5fbbec7670039f
Ashish Kr. Pandey <akp9117409798@gmail.com>
2024-04-30 23:58:36 +0530

    Home page completed with footer

--- app.js
+++ app.js
@@ -18,19 +18,19 @@ document.addEventListener('DOMContentLoaded', function () {
     async function fetch_products(url) {
         let data = await fetch(url);
         let response = await data.json();
-        console.log(response[0].title);
+        console.log();
 
         for (let i = 0; i < response.length; i++) {
             products.innerHTML += `<div
-            class="shadow-lg bg-gray-200 rounded-lg flex flex-col items-center gap-5 py-5 px-4 h-[500px] w-80"
+            class="shadow-lg bg-gray-200 rounded-lg flex flex-col items-center justify-between gap-5 py-5 px-4 h-full w-full"
           >
             <img
               class=" mix-blend-multiply h-48 w-48"
               src="${response[i].image}"
               alt=""
             />
-            <div class="flex flex-col items-center">
-              <div class="my-2 flex justify-between gap-2 text-2xl">
+            <div class="flex flex-col items-center justify-between">
+              <div class="my-2 flex justify-between gap-2 text-xl">
                 <div class="flex gap-2 relative overflow-hidden">
                   <i class="fa-regular fa-star text-yellow-500"></i>
                   <i class="fa-regular fa-star text-yellow-500"></i>
@@ -38,7 +38,7 @@ document.addEventListener('DOMContentLoaded', function () {
                   <i class="fa-regular fa-star text-yellow-500"></i>
                   <i class="fa-regular fa-star text-yellow-500"></i>
                   <div
-                    class="absolute top-0 left-0 flex gap-2 w-[${(parseInt(response[i].rating.rate))*20}%] overflow-hidden"
+                    class="absolute top-0 left-0 flex gap-2 w-[${(Number(response[i].rating.rate))*20}%] overflow-hidden"
                   >
                     <i class="fa-solid fa-star text-yellow-500"></i>
                     <i class="fa-solid fa-star text-yellow-500"></i>
@@ -54,10 +54,10 @@ document.addEventListener('DOMContentLoaded', function () {
               </h2>
               <p class="text-xl font-bold text-my-custom-color my-2">
                 ₹ ${response[i].price}
-                <del class="text-base text-gray-500 font-normal">₹  ${(Math.floor(response[i].price))+49}</del>
+                <del class="text-base text-gray-500 font-normal">₹  ${((response[i].price)+49).toFixed(2)}</del>
               </p>
             </div>
-            <div class="flex gap-4 items-center justify-center">
+            <div class="flex gap-4 items-between">
               <i
                 class="fa-solid fa-cart-plus px-4 py-2 cursor-pointer hover:bg-gray-200 transition ease-linear text-sm bg-white shadow-sm shadow-gray-400 rounded-md"
               ></i>
