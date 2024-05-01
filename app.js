6805a5df608289d630df182a8b659cd5f209a3d3
Ashish Kr. Pandey <akp9117409798@gmail.com>
2024-05-01 22:20:38 +0530

    nothing..

--- app.js
+++ app.js
@@ -1,27 +1,27 @@
 const menuBtn = document.querySelector("#menu-btn");
 const mobileNav = document.querySelector("#mobileNav");
 const crossNan = document.querySelector(".fa-xmark");
-
+const ShopCart = document.querySelector("#ShopCart");
 
 menuBtn.addEventListener("click", () => {
-    mobileNav.classList.remove("-translate-x-full", "opecity-0");
-    // mobileNav.classList.remove();
+  mobileNav.classList.remove("-translate-x-full", "opecity-0");
+  // mobileNav.classList.remove();
 });
 crossNan.addEventListener("click", () => {
-    mobileNav.classList.add("-translate-x-full", "opecity-0");
-    // mobileNav.classList.add();
+  mobileNav.classList.add("-translate-x-full", "opecity-0");
+  // mobileNav.classList.add();
 });
 
-document.addEventListener('DOMContentLoaded', function () {
-    let products = document.querySelector("#products");
-    console.log(products);
-    async function fetch_products(url) {
-        let data = await fetch(url);
-        let response = await data.json();
-        console.log();
+document.addEventListener("DOMContentLoaded", function () {
+  let products = document.querySelector("#products");
+  console.log(products);
+  async function fetch_products(url) {
+    let data = await fetch(url);
+    let response = await data.json();
+    console.log();
 
-        for (let i = 0; i < response.length; i++) {
-            products.innerHTML += `<div
+    for (let i = 0; i < response.length; i++) {
+      products.innerHTML += `<div
             class="shadow-lg bg-gray-200 rounded-lg flex flex-col items-center justify-between gap-5 py-5 px-4 h-full w-full"
           >
             <img
@@ -37,8 +37,8 @@ document.addEventListener('DOMContentLoaded', function () {
                   <i class="fa-regular fa-star text-yellow-500"></i>
                   <i class="fa-regular fa-star text-yellow-500"></i>
                   <i class="fa-regular fa-star text-yellow-500"></i>
-                  <div
-                    class="absolute top-0 left-0 flex gap-2 w-[${(Number(response[i].rating.rate))*20}%] overflow-hidden"
+                  <div style="width:${Number(response[i].rating.rate) * 20}%;"
+                    class="absolute top-0 left-0 flex gap-2 overflow-hidden"
                   >
                     <i class="fa-solid fa-star text-yellow-500"></i>
                     <i class="fa-solid fa-star text-yellow-500"></i>
@@ -54,7 +54,9 @@ document.addEventListener('DOMContentLoaded', function () {
               </h2>
               <p class="text-xl font-bold text-my-custom-color my-2">
                 ₹ ${response[i].price}
-                <del class="text-base text-gray-500 font-normal">₹  ${((response[i].price)+49).toFixed(2)}</del>
+                <del class="text-base text-gray-500 font-normal">₹  ${(
+                  response[i].price + (response[i].price * 21/100)
+                ).toFixed(2)}</del>
               </p>
             </div>
             <div class="flex gap-4 items-between">
@@ -67,8 +69,13 @@ document.addEventListener('DOMContentLoaded', function () {
                 Buy Now
               </button>
             </div>
-          </div>`
-        }
+          </div>`;
     }
-    fetch_products('https://fakestoreapi.com/products');
+  }
+  fetch_products("https://fakestoreapi.com/products");
 });
+
+ShopCart.addEventListener("click", function (e) {
+  let mainSection = document.querySelector("main");
+  mainSection.classList.toggle("hidden");
+})
